f46bf8537ddf9fec028504a422c7170e53a00d12
David Agaybi <davidagaybi@gmail.com>
2018-09-28 02:52:16 -0400

    Redux complete with addition of Redux Chrome extension

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -9,11 +9,15 @@ const initialState = {
 export default function(state = initialState, action) {
     switch(action.type) {
         case FETCH_POSTS:
-        console.log('Reducer...');
-        return {
-            ...state,
-            items: action.payload
-        }
+            return {
+                ...state,
+                items: action.payload
+            };
+        case NEW_POST:
+            return {
+                ...state,
+                item: action.payload
+            };
         default:
             return state;
 
